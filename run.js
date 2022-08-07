dbd69124c61b29ebbd3fcdffedd0aff066b9da9e
xubin <1tq-w3scsqyt@dingtalk.com>
2022-08-07 18:04:48 +0800

    change build path

--- run.js
+++ run.js
@@ -1,7 +1,6 @@
 const Webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./config/webpack.config.js');
-
 const compiler = Webpack(webpackConfig);
 const devServerOptions = { ...webpackConfig.devServer, open: true };
 const server = new WebpackDevServer(devServerOptions, compiler);
@@ -14,16 +13,27 @@ const runServer = async () => {
 };
 
 // 构建命令
-const build = async () => {
+const build = async (cwdPath) => {
     try {
         Webpack({
-            ...webpackConfig
+            ...webpackConfig,
+            output: {
+              // TODO 修改打包文件的出口
+              path: cwdPath + "/dist", // 输出文件路径
+              // 给打包过后的文件名字加上hash
+              // [contenthash:8] - 本应用打包输出文件级别的更新，导致输出文件名变化
+              filename: "[name]-[contenthash:8].js",
+          },
         },(err)=>{
+            let status = 'success'
             if (err){
+              status = 'fail'
                 console.log(err)
-                console.log('构建失败')
             }
-            console.log('构建成功')
+            process.send({ 
+              status,
+              err
+            });
         })
     } catch (e) {
         console.log(e, 'compiler')
@@ -31,8 +41,9 @@ const build = async () => {
 }
 process.on('message',message=>{
     const msg = JSON.parse( message )
+    const { cwdPath } = msg
     if (msg.type == 'build'){
-        build()
+        build(cwdPath)
     }
     if (msg.type == 'start'){
         runServer()
